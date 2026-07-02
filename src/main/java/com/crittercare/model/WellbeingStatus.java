8f14ea74d99e8d892ddfccd5feb6a85cbc122bd1
Phillip <philnowa77@gmail.com>
2026-07-02 20:15:34 +0200

    UI DB

---
+++ src/main/java/com/crittercare/model/WellbeingStatus.java
@@ -0,0 +1,10 @@
+package com.crittercare.model;
+
+/**
+ * Represents the current wellbeing state of an animal based on its vital stats.
+ */
+public enum WellbeingStatus {
+    GOOD,
+    WARNING,
+    CRITICAL
+}
